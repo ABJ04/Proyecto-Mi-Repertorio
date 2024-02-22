82689f2c0795255349588dce818d0a198171157d
Erich Armijo <eothandlock@gmail.com>
2024-02-22 02:31:51 -0300

    update: se crea archivo repertorio.json si no existe

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const path = require('path')
+app.use(express.json())
+app.listen(3000, console.log('¡Servidor encendido!'))
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')) })
+
+const archivo = 'repertorio.json'
+
+if (!fs.existsSync(archivo)) {
+  fs.writeFileSync(archivo, '[]')
+  console.log('El archivo repertorio.json ha sido creado.')
+} else {
+  console.log('El archivo repertorio.json ya existe.')
+}
